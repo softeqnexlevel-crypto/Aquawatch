971f06a9145f996209618e9bbb7adaae497c22b0
Evansrintari <rintari110@gmail.com>
2026-07-05 18:22:57 +0300

    added tthe config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000' || 'https://your-app.up.railway.app';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://aquawatchbackend.up.railway.app';
 
