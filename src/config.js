3775bc4ace42be6a95b74bcb76f159448ba7cf97
Evansrintari <rintari110@gmail.com>
2026-07-05 18:11:28 +0300

    adding config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000' || 'https://your-app.up.railway.app';
+
