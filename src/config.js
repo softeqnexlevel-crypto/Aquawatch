ada565699531f378a2da62fd6cf51f3e41266d49
Evansrintari <rintari110@gmail.com>
2026-08-14 11:00:36 +0300

    pushing the config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://aquawatchbackend.up.railway.app';
+// export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://aquawatchbackend.up.railway.app';
 
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://aquawatchbackend.onrender.com';
